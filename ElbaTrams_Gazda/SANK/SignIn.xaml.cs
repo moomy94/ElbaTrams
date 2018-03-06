8d074985bc0c4d21a778a324a65d347d9d30e4de
HovSkay <32835549+Hovsky@users.noreply.github.com>
2018-03-06 17:19:22 +0100

    Initial commit

---
+++ ElbaTrams_Gazda/SANK/SignIn.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SANK
+{
+    /// <summary>
+    /// Interaction logic for SignIN.xaml
+    /// </summary>
+    public partial class SignIn : Window
+    {
+        string username;
+        string password;
+        string user = "user";
+        string pass = "pass";
+        public SignIn()
+        {
+            InitializeComponent();
+        }
+
+        private void LogInButton_Click(object sender, RoutedEventArgs e)
+        {
+            password = PassBox.Password;
+            username = UserBox.Text;
+            bool jednako = String.Equals(password, pass, StringComparison.OrdinalIgnoreCase);
+            bool jednako2 = String.Equals(username, user, StringComparison.OrdinalIgnoreCase);
+            if (jednako&&jednako2)
+            {
+                SignedIn.Visibility=Visibility.Visible;
+                Application.Current.MainWindow.Show();
+                this.Hide();
+            }
+            else
+            {
+                Wrong.Visibility = Visibility.Visible;
+                PassBox.Password = "";
+                UserBox.Text = "";
+            }
+                
+        }
+
+        private void PassBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key==Key.Return)
+            {
+                password = PassBox.Password;
+                username = UserBox.Text;
+                bool jednako = String.Equals(password, pass, StringComparison.OrdinalIgnoreCase);
+                bool jednako2 = String.Equals(username, user, StringComparison.OrdinalIgnoreCase);
+                if (jednako && jednako2)
+                {
+                    SignedIn.Visibility = Visibility.Visible;
+                    Application.Current.MainWindow.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    Wrong.Visibility = Visibility.Visible;
+                    PassBox.Password = "";
+                    UserBox.Text = "";
+                }
+
+            }
+        }
+
+       
+    }
+}
